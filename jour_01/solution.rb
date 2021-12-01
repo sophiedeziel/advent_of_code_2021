2aed4b504a2f70ed28c437ba43204f9ae24a6c91
Sophie Déziel <courrier@sophiedeziel.com>
2021-12-01 18:36:14 -0500

    refactor pour utiliser la même fonction pour les deux parties

--- jour_01/solution.rb
+++ jour_01/solution.rb
@@ -1,18 +1,13 @@
 require 'pry'
 
-lines = File.open('input.txt', 'r').map { |line| line.to_i }
+@lines = File.open('input.txt', 'r').map { |line| line.to_i }
 
-previous = 0
-increases = lines.count do |measurement|  
-  increase = measurement > previous
-  previous = measurement
-  increase
-end - 1
-
-puts "partie 1: #{increases}"
-
-increases = lines.each_cons(4).count do |measurements|
-  measurements.last(3).sum > measurements.first(3).sum
+def increases(window_size)
+  @lines.each_cons(window_size + 1).count do |measurements|
+    measurements.last(window_size).sum > measurements.first(window_size).sum
+  end
 end
 
-puts "partie 2: #{increases}"
+puts "partie 1: #{increases(1)}"
+
+puts "partie 2: #{increases(3)}"
