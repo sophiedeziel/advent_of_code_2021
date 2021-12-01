907b84b441c76183c3d4f420b61ccd0facc8d42f
Sophie Déziel <courrier@sophiedeziel.com>
2021-12-01 18:33:35 -0500

    partie 2

--- jour_01/solution.rb
+++ jour_01/solution.rb
@@ -1,6 +1,7 @@
 require 'pry'
 
 lines = File.open('input.txt', 'r').map { |line| line.to_i }
+
 previous = 0
 increases = lines.count do |measurement|  
   increase = measurement > previous
@@ -9,3 +10,9 @@ increases = lines.count do |measurement|
 end - 1
 
 puts "partie 1: #{increases}"
+
+increases = lines.each_cons(4).count do |measurements|
+  measurements.last(3).sum > measurements.first(3).sum
+end
+
+puts "partie 2: #{increases}"
