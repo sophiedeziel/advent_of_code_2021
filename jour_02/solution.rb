4c114052edcaffa2a8c1d0a7de6983f8f85b5a3b
Sophie Déziel <courrier@sophiedeziel.com>
2021-12-02 12:36:04 -0500

    jour 2

---
+++ jour_02/solution.rb
@@ -0,0 +1,29 @@
+require 'pry'
+
+@lines = File.open('input.txt', 'r').map do |line| 
+  array = line.split(' ')
+  array[1] = array[1].to_i
+  array
+end
+
+horizontal = 0
+depth = 0
+aim = 0
+
+@lines.each do |c,v|
+  case c
+  when "down"
+    aim += v
+  when "up"
+    aim -= v
+  when "forward"
+    horizontal += v
+    depth += aim * v
+  end
+end
+
+puts "solution #1 #{horizontal * aim}"
+
+
+
+puts "solution #2 #{horizontal * depth}"
